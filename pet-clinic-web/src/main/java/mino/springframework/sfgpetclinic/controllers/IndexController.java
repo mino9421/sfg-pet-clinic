34006740d59b4a28b1d4920425d052c9b27e8d42
Mino <mino9421@gmail.com>
2022-08-26 22:41:21 -0400

    added Owner index page and controller. Closes #13

--- pet-clinic-web/src/main/java/mino/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/mino/springframework/sfgpetclinic/controllers/IndexController.java
@@ -2,11 +2,11 @@ package mino.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
-
-    @RequestMapping({"", "/", "index", "index.html"})
+    @RequestMapping(value = {"", "/", "index", "index.html"}, method = RequestMethod.GET)
     public String index() {
 
         return "index";
